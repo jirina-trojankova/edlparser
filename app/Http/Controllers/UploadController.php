c329de5e2aa32ff8bc4b03b07e798a428e235b43
jirina-trojankova <jirina.trojankova@seznam.cz>
2018-06-07 11:24:48 +0200

    Store function uploads and shows files

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Input;
 
 class UploadController extends Controller
 {
@@ -12,24 +14,46 @@ class UploadController extends Controller
         return view ('welcome');
     }
 
-    public function store(request $request)
-    {
-        if ($request->hasFile('file')){
-        $request->file('file');
-        // $path = $request->file->store('public');
-        // return view ('store',["path"=>$path]);
-        // return Storage::putFile('public', $request->file('file'));
-        return $request->file->storeAs('public','jana.jpg');
-        }else{
+    public function store (){
 
-            return 'No file selected';
+        If(Input::hasFile('file')){
+            
+
+            $file = Input::file('file');
+
+            $destinationPath = public_path(). '/uploads/';
+            $filename = $file->getClientOriginalName();
+
+            $file->move($destinationPath, $filename);
+
+            //echo  $filename;
+            echo '<img src="uploads/'. $filename . '"/>';
         }
     }
 
-    public function show(){
-        $url = Storage::url('jana.jpg');
-        return  "<img src='".$url."' />";
-        // return Storage::files('public');
-    }
+    // public function store(request $request)
+    // {
+    //     if ($request->hasFile('file')){
+    //     $request->file('file');
+    //     // $path = $request->file->store('public');
+    //     // return view ('store',["path"=>$path]);
+    //     // Storage::disk('public')->put($request->getClientOriginalName() . $request->getClientOriginalExtension() ,$request->file);
+    //     // return Storage::putFile('public', $request->file('file'));
+    //     $request->file->store('public');
+    //     $path = $request->file->getClientOriginalName();
+    //     // $request->file->path();
+    //     return "<img src='/storage/".$path."' />";
+    //     // <img src="{{ 'storage/' . $path }}">
+    //     }else{
+
+    //         return 'No file selected';
+    //     }
+    // }
+
+    // public function show(){
+    //     $url = Storage::url('jana.jpg');
+    //     return  "<img src='".$url."' />";
+    //     // return Storage::files('public');
+    // }
 
 }
