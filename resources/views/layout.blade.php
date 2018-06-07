641869a7badd80c9197613bbb8d06f7ab5f79878
jirina-trojankova <jirina.trojankova@seznam.cz>
2018-06-07 15:20:40 +0200

    Form moved to layout.blade

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -90,7 +90,16 @@
         <div class="title">
             EDL Parser
         </div>
-      
+        <div class="strong">
+                {!! Form::open(['url' => '/store', 'method' => 'post', 'files' => true]) !!} 
+                {!! 'Epizode name: ' !!}
+                {!! Form::text('name') !!}  
+                <br>
+                {!! Form::file('file'); !!} 
+                {!! Form::submit('Upload File') !!} 
+                {!! Form::token() !!}
+                {!! Form::close() !!}
+        </div>
         @yield('content')
     </div>
 </body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,6 @@
 @extends('layout')
 
     @section('content')
-    <div class="strong">
-            {!! Form::open(['url' => '/store', 'method' => 'post', 'files' => true]) !!} 
-            {!! 'Epizode name: ' !!}
-            {!! Form::text('name') !!}  
-            <br>
-            {!! Form::file('file'); !!} 
-            {!! Form::submit('Upload File') !!} 
-            {!! Form::token() !!}
-            {!! Form::close() !!}
-    </div>
+  
     @endsection
     
